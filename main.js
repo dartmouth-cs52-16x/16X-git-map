f45ca6a37cce9aebbb5aceb7dcbafbb18e02130e
Tim Tregubov <tim@zingweb.com>
2016-12-16 23:07:11 -0500

    fixing safari

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
 /* SETUP */
-const map = L.map('map',{
+var map = L.map('map',{
     tms: false
 }).setView([42.755942, -72.8092041],3);
 
-const PersonIcon = L.Icon.extend({
+var PersonIcon = L.Icon.extend({
     options: {
         iconSize: [
             60, 60
@@ -15,13 +15,13 @@ const PersonIcon = L.Icon.extend({
 });
 
 // load up the background tile layer
-const Stamen_Watercolor = L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg', {}).addTo(map);
+var Stamen_Watercolor = L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg', {}).addTo(map);
 
 // all the facemarkers will go into one layer
-const facemarkers = L.layerGroup();
+var facemarkers = L.layerGroup();
 
 // function to add markers
-const addMarker = function(options) {
+var addMarker = function(options) {
   var icon = new PersonIcon({iconUrl: options.iconUrl});
   var marker = L.marker(options.lat_long, {icon: icon, url: options.url, name: options.name})
   facemarkers.addLayer(marker.addTo(map).bindPopup(options.message));
